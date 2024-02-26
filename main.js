e143167953c732a8b38728491bcc4e198a1d606c
HectorM12 <90624620+HectorM12@users.noreply.github.com>
2024-02-26 11:00:26 -0500

    done

---
+++ main.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'They are socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.vuemastery.com/',
+            inventory: 100,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyster'],
+            variants: {
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'}
+            }
+        }
+    },
+    methods: {
+addToCart() {
+    this.cart += 1;
+}
+removeFromCart()
+    }
+})
